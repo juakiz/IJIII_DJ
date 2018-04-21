65e66abcda8404c19a070a614972337d54e427af
juakiz <juakiz@gmail.com>
2018-04-21 19:32:10 +0100

    Level Editor

--- src/states/Game.js
+++ src/states/Game.js
@@ -5,7 +5,13 @@ import Geom from '../geom'
 import Platform from '../objects/platform'
 
 const LEVELS = {
-   level1: [
+  bounds: [
+    [0, 29, 20, 1],
+    [0, 0, 20, 1],
+    [0, 1, 1, 28],
+    [19, 1, 1, 28],
+  ],
+  level1: [
     [0, 29, 20, 1],
     [0, 0, 20, 1],
     [0, 1, 1, 28],
@@ -19,7 +25,7 @@ const LEVELS = {
     [15, 12, 4, 1],
   ],
   level2: [
-
+    [6, 4, 5, 2],
   ],
 }
 
@@ -37,7 +43,7 @@ export default class extends Phaser.State {
 
     this.ground = this.game.add.group()
 
-    this.bluePrint('level1');
+    this.bluePrint('level2');
 
     this.game.add.existing(this.player)
   }
@@ -47,6 +53,10 @@ export default class extends Phaser.State {
     const opt = { bgColor: 0x000000 };
     for (let i = 0; i < LEVEL.length; i++) {
       this.ground.add(new Platform(this.game, LEVEL[i][0], LEVEL[i][1], LEVEL[i][2], LEVEL[i][3], opt))
+    }
+    const BOUNDS = LEVELS['bounds'];
+    for (let i = 0; i < BOUNDS.length; i++) {
+      this.ground.add(new Platform(this.game, BOUNDS[i][0], BOUNDS[i][1], BOUNDS[i][2], BOUNDS[i][3], opt))
     }
   }
 
