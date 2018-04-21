38b3397fb46b7cf638da29a4ec0fe3f196d0c496
juakiz <juakiz@gmail.com>
2018-04-21 18:15:21 +0100

    Player encapsilation

--- src/objects/player.js
+++ src/objects/player.js
@@ -5,15 +5,68 @@ export default class extends Phaser.Sprite {
     super(game, x, y, asset)
     this.anchor.setTo(0, 1)
 
-    const _state = this.game.state.getCurrentState()
+    this.MAX_SPEED = 400
+    this.ACCELERATION = 1000
+    this.DRAG = 600
+    this.GRAVITY = 2600
+    this.JUMP_SPEED = -750
+
+    this._state = this.game.state.getCurrentState()
+
     this.height = 128;
     this.width = 64;
+
     this.game.physics.enable(this, Phaser.Physics.ARCADE)
-    this.body.maxVelocity.setTo(_state.MAX_SPEED, _state.MAX_SPEED * 10)
-    this.body.drag.setTo(_state.DRAG, 0)
+    this.body.maxVelocity.setTo(this.MAX_SPEED, this.MAX_SPEED * 10)
+    this.body.drag.setTo(this.DRAG, 0)
     this.body.collideWorldBounds = true
+
+    this.game.input.keyboard.addKeyCapture([
+      Phaser.Keyboard.LEFT,
+      Phaser.Keyboard.RIGHT,
+      Phaser.Keyboard.UP,
+      Phaser.Keyboard.DOWN,
+      Phaser.Keyboard.SPACEBAR
+    ])
+    this.body.velocity.x = -1;
+  }
+
+  create() {
+    
   }
 
   update () {
+    this.game.physics.arcade.collide(this, this._state.ground)
+
+    if (this.leftInputIsActive()) {
+      this.body.velocity.x = -this.MAX_SPEED
+    } else if (this.rightInputIsActive()) {
+      this.body.velocity.x = this.MAX_SPEED
+    } else {
+      this.body.velocity.x = 0
+    }
+
+    if (this.body.touching.down && this.upInputIsActive(5)) {
+      // Jump when the player is touching the ground and the up arrow is pressed
+      this.body.velocity.y = this.JUMP_SPEED
+    }
+  }
+
+  leftInputIsActive () {
+    let isActive = false
+    isActive = this._state.input.keyboard.isDown(Phaser.Keyboard.LEFT)
+    return isActive
+  }
+
+  rightInputIsActive () {
+    let isActive = false
+    isActive = this._state.input.keyboard.isDown(Phaser.Keyboard.RIGHT)
+    return isActive
+  }
+
+  upInputIsActive () {
+    let isActive = false
+    isActive = this._state.input.keyboard.downDuration(Phaser.Keyboard.SPACEBAR)
+    return isActive
   }
 }
--- src/states/Game.js
+++ src/states/Game.js
@@ -17,22 +17,12 @@ export default class extends Phaser.State {
 
   create () {
     this.game.stage.backgroundColor = 0x4488cc
-    this.MAX_SPEED = 400
-    this.ACCELERATION = 1000
-    this.DRAG = 600
     this.GRAVITY = 2600
-    this.JUMP_SPEED = -750
 
     this.player = new Player(this.game, 32, 960 - 32, 'mushroom')
 
     this.game.physics.arcade.gravity.y = this.GRAVITY
-    this.game.input.keyboard.addKeyCapture([
-      Phaser.Keyboard.LEFT,
-      Phaser.Keyboard.RIGHT,
-      Phaser.Keyboard.UP,
-      Phaser.Keyboard.DOWN,
-      Phaser.Keyboard.SPACEBAR
-    ])
+
     this.ground = this.game.add.group()
 
     this.bluePrint();
@@ -45,41 +35,6 @@ export default class extends Phaser.State {
     for (let i = 0; i < LEVEL.length; i++) {
       this.ground.add(new Platform(this.game, LEVEL[i][0], LEVEL[i][1], LEVEL[i][2], LEVEL[i][3], opt))
     }
-  }
-
-  update () {
-    this.game.physics.arcade.collide(this.player, this.ground)
-
-    if (this.leftInputIsActive()) {
-      this.player.body.velocity.x = -this.MAX_SPEED
-    } else if (this.rightInputIsActive()) {
-      this.player.body.velocity.x = this.MAX_SPEED
-    } else {
-      this.player.body.velocity.x = 0
-    }
-
-    if (this.player.body.touching.down && this.upInputIsActive(5)) {
-      // Jump when the player is touching the ground and the up arrow is pressed
-      this.player.body.velocity.y = this.JUMP_SPEED
-    }
-  }
-
-  leftInputIsActive () {
-    let isActive = false
-    isActive = this.input.keyboard.isDown(Phaser.Keyboard.LEFT)
-    return isActive
-  }
-
-  rightInputIsActive () {
-    let isActive = false
-    isActive = this.input.keyboard.isDown(Phaser.Keyboard.RIGHT)
-    return isActive
-  }
-
-  upInputIsActive () {
-    let isActive = false
-    isActive = this.input.keyboard.downDuration(Phaser.Keyboard.SPACEBAR)
-    return isActive
   }
 
   render () {
