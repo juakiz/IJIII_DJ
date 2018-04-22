ec2335e9a003e5efb0b45a37d44fdde7f1c85945
Jean Carlos A. Ortiz <ayalajeancarlos.91@gmail.com>
2018-04-22 01:47:45 +0100

    game over

--- src/states/GameOver.js
+++ src/states/GameOver.js
@@ -6,29 +6,32 @@ export default class extends Phaser.State {
     this.game.stage.backgroundColor = 0x4488cc
     let textStyle = {font: '54px Helvetica', align: 'center', fill: 'white'};
     
-    let title = this.game.add.text(this.game.world.centerX, this.game.world.centerY - 100, 'Final Turbo Dialog Jamp', textStyle);
+    let title = this.game.add.text(this.game.world.centerX, this.game.world.centerY - 100, 'You lost your mind', textStyle);
     title.anchor.set(0.5);
 
     textStyle.font = '40px Helvetica'
         
-    let instructions = this.game.add.text(this.game.world.centerX, this.game.world.centerY, 'Press Enter to start', textStyle);
+    let instructions = this.game.add.text(this.game.world.centerX, this.game.world.centerY, 'Press "R" to retry\nPress "E" to easy mode', textStyle);
     instructions.anchor.set(0.5);
-    
-    textStyle.font = '24px Helvetica'
-    
-    let controlMessage = this.game.add.text(this.game.world.centerX, this.game.world.centerY + 150, 'left arrow to move left\nright arrow to move right\nspacebar to jump.', textStyle);
-    controlMessage.anchor.set(0.5);
     
     textStyle.font = '12px Helvetica'
 
     let muteMessage = this.game.add.text(this.game.world.centerX, this.game.world.centerY + 400, 'the developers of this game are not responsible for\ncholera attacks, spontaneous rage, cranial blood pressure spikes, pre or post game stress,\nor any adverse medical conditions arising from the practice of this game.', textStyle);
     muteMessage.anchor.set(0.5);
        
-    let enterKey = this.game.input.keyboard.addKey(Phaser.KeyCode.ENTER);
-    enterKey.onDown.addOnce( () => {
+    let rKey = this.game.input.keyboard.addKey(Phaser.KeyCode.R);
+    rKey.onDown.addOnce( () => {
       this.game.state.start('Game')
       this.game.myProps = {
         difficulty: 0
+      }
+    });
+    
+    let eKey = this.game.input.keyboard.addKey(Phaser.KeyCode.E);
+    eKey.onDown.addOnce( () => {
+      this.game.state.start('Game')
+      this.game.myProps = {
+        difficulty: 1
       }
     });
   }
