322b6b518cbfa97f4d941e514d1ad22096560841
juakiz <juakiz@gmail.com>
2018-04-22 02:58:49 +0100

    Menu text

--- src/states/Menu.js
+++ src/states/Menu.js
@@ -4,27 +4,74 @@ import { centerGameObjects } from '../utils'
 export default class extends Phaser.State {
   create() {
     this.game.stage.backgroundColor = 0x4488cc
-    let textStyle = {font: '54px Helvetica', align: 'center', fill: 'white'};
+    let textStyle = {font: '50px Helvetica', align: 'center', fill: 'white'};
     
-    let title = this.game.add.text(this.game.world.centerX, this.game.world.centerY - 100, 'Final Turbo Dialog Jamp', textStyle);
+    let title = this.game.add.text(
+      this.game.world.centerX,
+      260,
+      'Final Turbo Dialog Jamp',
+      textStyle);
     title.anchor.set(0.5);
 
-    textStyle.font = '40px Helvetica'
+    textStyle.font = '36px Helvetica'
         
-    let instructions = this.game.add.text(this.game.world.centerX, this.game.world.centerY, 'Press Enter to start', textStyle);
+    let instructions = this.game.add.text(
+      this.game.world.centerX,
+      this.game.world.centerY + 60,
+      'Press SPACEBAR to start!',
+      textStyle);
     instructions.anchor.set(0.5);
+
+    this.game.time.events.loop(400, () => { instructions.alpha = instructions.alpha ? 0 : 1; }, this);
     
     textStyle.font = '24px Helvetica'
     
-    let controlMessage = this.game.add.text(this.game.world.centerX, this.game.world.centerY + 150, 'left arrow to move left\nright arrow to move right\nspacebar to jump.', textStyle);
+    let controlMessage = this.game.add.text(
+      this.game.world.centerX,
+      this.game.world.centerY + 228,
+      'left arrow to move left\nright arrow to move right\nspacebar to jump.',
+      textStyle);
     controlMessage.anchor.set(0.5);
+    controlMessage.align = 'left';
+
+    controlMessage.addColor('#ffff00', 0);
+    controlMessage.addColor('#ffffff', 4);
+
+    controlMessage.addColor('#ffff00', 23);
+    controlMessage.addColor('#ffffff', 28);
+
+    controlMessage.addColor('#ffff00', 48);
+    controlMessage.addColor('#ffffff', 56);
     
     textStyle.font = '12px Helvetica'
 
-    let muteMessage = this.game.add.text(this.game.world.centerX, this.game.world.centerY + 400, 'the developers of this game are not responsible for\ncholera attacks, spontaneous rage, cranial blood pressure spikes, pre or post game stress,\nor any adverse medical conditions arising from the practice of this game.', textStyle);
-    muteMessage.anchor.set(0.5);
+    // let muteMessage = this.game.add.text(
+    //   this.game.world.centerX,
+    //   this.game.world.centerY + 400,
+    //   'the developers of this game are not responsible for\ncholera attacks, spontaneous rage,' +
+    //   ' cranial blood pressure spikes, pre or post game stress,\nor any adverse medical conditions arising from the practice of this game.',
+    //   textStyle);
+    // muteMessage.anchor.set(0.5);
+
+    // muteMessage.fill = '#CCCCCC';
+
+    let muteMessage = this.game.add.text(
+      this.game.world.centerX,
+      960,
+      'the developers of this game are not responsible for' + 
+      'cholera attacks, spontaneous rage, cranial blood pressure spikes, pre or post game stress,' + 
+      'or any adverse medical conditions arising from the practice of this game.',
+      textStyle);
+    muteMessage.anchor.set(0.5, 1);
+
+    muteMessage.align = 'center';
+    muteMessage.wordWrap = true;
+    muteMessage.wordWrapWidth = 640;
+    muteMessage.lineSpacing = -8;
+
+    muteMessage.fill = '#CCCCCC';
        
-    let enterKey = this.game.input.keyboard.addKey(Phaser.KeyCode.ENTER);
+    let enterKey = this.game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
     enterKey.onDown.addOnce( () => {
       this.game.state.start('Game')
       this.game.myProps = {
